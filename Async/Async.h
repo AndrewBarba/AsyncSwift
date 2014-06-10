de5cec90237b3da58e9f14c68b2602439f7dd041
Andrew Barba <abarba.77@gmail.com>
2014-06-09 22:51:04 -0400

    Initial Commit

---
+++ Async/Async.h
@@ -0,0 +1,19 @@
+//
+//  Async.h
+//  Async
+//
+//  Created by Andrew Barba on 6/9/14.
+//  Copyright (c) 2014 Andrew Barba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Async.
+FOUNDATION_EXPORT double AsyncVersionNumber;
+
+//! Project version string for Async.
+FOUNDATION_EXPORT const unsigned char AsyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Async/PublicHeader.h>
+
+
